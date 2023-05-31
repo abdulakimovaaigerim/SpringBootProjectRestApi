2f8157d0f5f165f4518b844a399893d6db84a901
Марьям <abdulakimovaaigerim205@gmail.com>
2023-05-31 19:37:18 +0600

    Initial commit

---
+++ src/main/java/peaksoft/SpringBootProjectRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojectrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/SpringBootProjectRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootprojectrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
